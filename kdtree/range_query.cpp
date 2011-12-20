41f2561a97841b9cd31d5aea1d944fd6d097b94c
zyd_com <zyd_com@b7896322-a1cb-4877-ae01-239c6ac07206>
2011-12-20 15:16:42 +0000

    add range query
    git-svn-id: http://code.taobao.org/svn/mspy/trunk@19 b7896322-a1cb-4877-ae01-239c6ac07206

--- kdtree/range_query.cpp
+++ kdtree/range_query.cpp
@@ -12,28 +12,30 @@ double get_interval(struct timeval start, struct timeval end)
 
     uint64_t val = (end.tv_sec - start.tv_sec) * 1000000 +
         (end.tv_usec - start.tv_usec);
-    return (double)val / 1000;  // ms
+    return (double)val / 1000;
 }
 
-inline bool equals(file_meta *m, const meta_info_t &c)
+inline bool encloses(file_meta *m, const meta_info_t &vmin, const meta_info_t &vmax)
 {
-     if(m->uid == c.d[2] && m->ino == c.d[0] && 
-           m->size == c.d[1] && m->atime == c.d[3] && 
-            m->ctime == c.d[4] && m->mtime == c.d[5]) {
-            return true;
-     }
-     return false;
+    if(m->ino >= vmin.d[0] && m->ino <= vmax.d[0] &&
+       m->size >= vmin.d[1] && m->size <= vmax.d[1] &&
+       m->uid >= vmin.d[2] && m->uid <= vmax.d[2] && 
+       m->atime >= vmin.d[3] && m->atime <= vmax.d[3] &&
+       m->ctime >= vmin.d[4] && m->ctime <= vmax.d[4] &&
+       m->mtime >= vmin.d[5] && m->mtime <= vmax.d[5]) {
+        return true;
+    }
+    return false;
 }
 
-file_meta *mylist_find(mylist *mlist, const meta_info_t &c)
+void mylist_find_range(mylist *mlist, const meta_info_t &vmin, const meta_info_t &vmax, std::vector<file_meta *> &result)
 {
     for(int i = 0; i < mlist->size; i++) {
         file_meta *m = (file_meta *)mlist->item[i].data;
-        if(true == equals(m, c)) {
-            return m;
+        if(true == encloses(m, vmin, vmax)) {
+            result.push_back(m);
         }
    }
-   return NULL;
 }
 
 void write_xls(const char * filename, double result[][100], int rows, int cols)
@@ -88,26 +90,30 @@ int main(int argc, char *argv[])
             se.insert(mi);
         }
 
+        meta_info_t min(0, 0, 0, 1024 * 1024, 0, 0, 1209492430);
+        meta_info_t max(0xffffffff, 0, 0, 2 * 1024 * 1024, 0xffffffff, 0xffffffff, 0xffffffff);
+        region_t reg(min, max);
+        std::vector<meta_info_t *> v;
+        std::vector<file_meta *> r;
 
-        file_meta *m = (file_meta *)mlist->item[base/2-1].data;
-        meta_info_t *target = new meta_info_t(m->ino, m->uid, m->gid, m->size, m->atime, m->ctime, m->mtime);
-        
+
         // list
         gettimeofday(&start, NULL);
-        mylist_find(mlist, *target);
+        mylist_find_range(mlist, min, max, r);
         gettimeofday(&end, NULL);
         result[0][idx] = get_interval(start, end);
 
         // kdtree
         gettimeofday(&start, NULL);
-        se.find_exact(*target);
+        se.find_within_range(reg, v);
         gettimeofday(&end, NULL);
         result[1][idx] = get_interval(start, end);
         
         // balanced kdtree
         se.balance();
+        v.clear();
         gettimeofday(&start, NULL);
-        se.find_exact(*target);
+        se.find_within_range(reg, v);
         gettimeofday(&end, NULL);
         result[2][idx] = get_interval(start, end);
 
